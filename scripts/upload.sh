4ebcb6abf13baa479356a9f57dfd2a45098564f0
oliveiraallex <39618015+oliveiraallex@users.noreply.github.com>
2019-02-07 01:18:34 +0100

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,11 +2,16 @@
 
 set -ex
 
-zip -qr download/Pharo7.0-32bit-iot-$(date +%Y%m%d%H%M).zip  Pharo7.0-32bit-iot/
-rm -rf Pharo7.0-32bit-iot
+FILE_DATE=(date +%Y%m%d%H%M)
+FILE_PHARO=Pharo7.0-32bit-iot
+FILE_NAME=$FILE_PHARO-$FILE_DATE.zip
+
+zip -qr download/$FILE_NAME  $FILE_PHARO
+cp download/$FILE_NAME download/$FILE_PHARO-latest.zip
+rm -rf $FILE_PHARO
 
 git checkout master
 git add download
 git add -u
-git commit -m "Travis upload $(date +%Y%m%d%H%M)"
+git commit -m "Travis upload $FILE_DATE"
 git push http://oliveiraallex:${GH}@github.com/oliveiraallex/pharothings-ci.git > /dev/null 2>&1
