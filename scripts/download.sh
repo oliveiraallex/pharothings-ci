49ea513586dcdf2ba49fa58af1d8cf4e749a03e4
Allex Oliveira <allexoliveira@MacBook-Air.local>
2019-02-07 11:39:29 +0100

    add configure file

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,11 +2,13 @@
 
 set -ex
 
+FILE_PHARO=Pharo7.0-32bit-iot
+
 # PharoVM 7.0 for Linux 32bit: 
 wget "https://files.pharo.org/get-files/70/pharo-linux-stable.zip"
-unzip pharo-linux-stable.zip -d Pharo7.0-32bit-iot
+unzip pharo-linux-stable.zip -d $FILE_PHARO
 rm -rf pharo-linux-stable.zip
-cd Pharo7.0-32bit-iot 
+cd $FILE_PHARO
 # Pharo image 32bit:
 wget "https://files.pharo.org/get-files/70/pharo.zip"
 unzip -o pharo.zip
