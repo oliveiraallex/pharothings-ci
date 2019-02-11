060d7d5ff6adb826809ca5ee90a5d6178135cb2e
oliveiraallex <39618015+oliveiraallex@users.noreply.github.com>
2019-02-12 00:07:53 +0100

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,9 +7,9 @@ TEMP_RASP_SERCLI=pharo7-iot-rasp-ser-cli
 TEMP_MULTI_SERCLI=pharo7-iot-multi-ser-cli
 FILE_DATE=$(date +%Y%m%d%H%M)
 
-zip -qr $TEMP_RASP_SER download/$TEMP_RASP_SER-$FILE_DATE.zip
-zip -qr $TEMP_RASP_SERCLI download/$TEMP_RASP_SERCLI-$FILE_DATE.zip
-zip -qr $TEMP_MULTI_SERCLI download/$TEMP_MULTI_SERCLI-$FILE_DATE.zip
+zip -qr download/$TEMP_RASP_SER-$FILE_DATE.zip $TEMP_RASP_SER
+zip -qr download/$TEMP_RASP_SERCLI-$FILE_DATE.zip $TEMP_RASP_SERCLI
+zip -qr download/$TEMP_MULTI_SERCLI-$FILE_DATE.zip $TEMP_MULTI_SERCLI
 
 rm -rf $TEMP_RASP_SER TEMP_RASP_SERCLI TEMP_MULTI_SERCLI
 
