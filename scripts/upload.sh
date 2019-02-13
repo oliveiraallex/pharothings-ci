cb02866ee9f51b2176985804b6f95f7875820b4b
Allex Oliveira <allexoliveira@MacBook-Air.local>
2019-02-13 17:55:52 +0100

    Upload

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,7 +7,7 @@ TEMP_RASP_SERCLI=pharo7-iot-rasp-ser-cli
 TEMP_MULTI_SERCLI=pharo7-iot-multi-ser-cli
 FILE_DATE=$(date +%Y%m%d%H%M)
 
-rm -rf download/*
+rm -rf download/*.*
 
 zip -qr download/$TEMP_RASP_SER-$FILE_DATE.zip $TEMP_RASP_SER
 zip -qr download/$TEMP_RASP_SERCLI-$FILE_DATE.zip $TEMP_RASP_SERCLI
