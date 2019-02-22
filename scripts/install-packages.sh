50853079f06255f12ce54a0fe5950da876f7426f
Allex Oliveira <allex.oliveira@yahoo.com.br>
2019-02-22 15:08:53 +0100

    Removing Iceberg of Raspberry Server and Client

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -18,20 +18,23 @@ mkdir -p tmp/pharo64 && cd tmp/pharo64
 wget -O - get.pharo.org/64/vm70 | bash
 cd ../..
 
-# 1.2 Pharo 7 IoT Raspberry Server
+# Preparing Pharo 32 bit image
 mkdir -p tmp/$TEMP_RASP_SER && cd tmp/$TEMP_RASP_SER
 wget -O - get.pharo.org/70 | bash
 mv Pharo.image PharoThings32.image
 mv Pharo.changes PharoThings32.changes
 cd ../..
+
+# 1.2 Pharo 7 IoT Raspberry Server
 ./tmp/pharo32/pharo tmp/$TEMP_RASP_SER/PharoThings32.image eval "
-Iceberg enableMetacelloIntegration: true.
-$1 Metacello new
+$1 Iceberg enableMetacelloIntegration: true.
+Metacello new
   baseline: 'PharoThings';
   repository: 'github://pharo-iot/PharoThings/src';
   load: #(RemoteDevServer Raspberry). $1
 Smalltalk saveSession. 
-"   > /dev/null 2>&1
+"   
+# > /dev/null 2>&1
 mkdir $TEMP_RASP_SER
 cp tmp/$TEMP_RASP_SER/PharoThings32.image $TEMP_RASP_SER
 cp tmp/$TEMP_RASP_SER/PharoThings32.changes $TEMP_RASP_SER
@@ -43,13 +46,14 @@ cp tmp/pharo-server $TEMP_RASP_SER
 # 1.3 Pharo 7 IoT Raspberry Server Client
 cp -r tmp/$TEMP_RASP_SER/ tmp/$TEMP_RASP_SERCLI/
 ./tmp/pharo32/pharo tmp/$TEMP_RASP_SERCLI/PharoThings32.image eval "
-Iceberg enableMetacelloIntegration: true.
-$1 Metacello new
+$1 Iceberg enableMetacelloIntegration: true.
+Metacello new
   baseline: 'PharoThings';
   repository: 'github://pharo-iot/PharoThings/src';
   load: 'RemoteDev'. $1
 Smalltalk saveSession. 
-"  > /dev/null 2>&1
+" 
+# > /dev/null 2>&1
 mkdir $TEMP_RASP_SERCLI
 cp tmp/$TEMP_RASP_SERCLI/PharoThings32.image $TEMP_RASP_SERCLI
 cp tmp/$TEMP_RASP_SERCLI/PharoThings32.changes $TEMP_RASP_SERCLI
@@ -57,17 +61,71 @@ cp tmp/$TEMP_RASP_SERCLI/Pharo*.sources $TEMP_RASP_SERCLI
 cp tmp/pharo $TEMP_RASP_SERCLI
 cp tmp/pharo-ui $TEMP_RASP_SERCLI
 cp tmp/pharo-server $TEMP_RASP_SERCLI
-
-# 1.4 Pharo 7 IoT Multiplataform Server Client
+# Coping files from Raspberry server client folder to Multiplatform
 cp -r tmp/$TEMP_RASP_SERCLI/ tmp/$TEMP_MULTI_SERCLI/
+
+# Removing Iceberg from Raspberry Server and Server Client image
+remove_iceberg() {
+    VERSION=$1
+    ./tmp/pharo32/pharo $VERSION/PharoThings32.image eval "
+$2 LGitLibrary shutDown: true.
+IceMetacelloRepositoryAdapter allInstances do: #unregister.
+MetacelloPharoPlatform select.
+#(
+    'BaselineOfTonel'
+    'BaselineOfLibGit'
+    'BaselineOfIceberg'
+    'MonticelloTonel-Core'
+    'MonticelloTonel-FileSystem'
+    'MonticelloTonel-Tests'
+    'Iceberg-UI' 
+    'Iceberg-TipUI'
+    'Iceberg-Plugin-Pharo' 
+    'Iceberg-Plugin-Metacello' 
+    'Iceberg-Plugin-GitHub'
+    'Iceberg-Plugin-Migration'
+    'Iceberg-Plugin' 
+    'Iceberg-Metacello-Integration' 
+    'Iceberg-Libgit-Tonel' 
+    'Iceberg-Libgit-Filetree' 
+    'Iceberg-Libgit' 
+    'Iceberg-Tests'
+    'Iceberg-Memory'
+    'Iceberg-UI-Tests'
+    'Iceberg-Core' 
+    'Iceberg-Changes' 
+    'Iceberg-Adapters' 
+    'Iceberg'
+    'Iceberg-GitCommand'
+    'Iceberg-SmartUI'
+    'Iceberg-Pharo6'
+    'LibGit-Core') 
+do: [ :each |
+    (each asPackageIfAbsent: [ 
+        (each , ' not found') logCr.
+        nil ])
+            ifNotNil: [ :package |
+                ('Removing ', each) logCr.
+                package removeFromSystem ] ]. $2
+Smalltalk saveSession. 
+"  
+# > /dev/null 2>&1
+rm -rf $VERSION/pharo-local
+} 
+remove_iceberg "$TEMP_RASP_SER"
+remove_iceberg "$TEMP_RASP_SERCLI"
+
+# Preparing Pharo 64 bit image
 cd tmp/$TEMP_MULTI_SERCLI/
 wget -O - get.pharo.org/64/70 | bash
 mv Pharo.image PharoThings64.image
 mv Pharo.changes PharoThings64.changes
 cd ../..
+
+# 1.4 Pharo 7 IoT Multiplataform Server Client
 ./tmp/pharo64/pharo tmp/$TEMP_MULTI_SERCLI/PharoThings64.image eval "
-Iceberg enableMetacelloIntegration: true.
-$1 Metacello new
+$1 Iceberg enableMetacelloIntegration: true.
+Metacello new
   baseline: 'PharoThings';
   repository: 'github://pharo-iot/PharoThings/src';
   load: 'RemoteDev'.
@@ -76,7 +134,8 @@ Metacello new
   repository: 'github://pharo-iot/PharoThings/src';
   load: #(RemoteDevServer Raspberry). $1
 Smalltalk saveSession. 
-"  > /dev/null 2>&1
+" 
+# > /dev/null 2>&1
 mkdir $TEMP_MULTI_SERCLI
 cp tmp/$TEMP_MULTI_SERCLI/PharoThings32.image $TEMP_MULTI_SERCLI
 cp tmp/$TEMP_MULTI_SERCLI/PharoThings32.changes $TEMP_MULTI_SERCLI
