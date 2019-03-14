47afd56c0007e33616bf0b0689f1c62189b662ea
Allex Oliveira <allex.oliveira@yahoo.com.br>
2019-03-14 17:02:56 +0100

    fix upload process

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -11,4 +11,4 @@ git config --global user.email "allex.oliveira@yahoo.com.br"
 git config --global user.name "Allex Oliveira"
 git add docs/*.zip
 git commit -m "Travis upload"
-git push http://oliveiraallex:${GH}@github.com/oliveiraallex/pharothings-ci.git 
+git push http://oliveiraallex:${GH}@github.com/oliveiraallex/pharothings-ci.git
