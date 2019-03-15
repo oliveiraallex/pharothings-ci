d3709b8086f8e6e62cf53d6e0ae90a4d1656fbfc
Allex Oliveira <allex.oliveira@yahoo.com.br>
2019-03-15 17:55:33 +0100

    fix runscripts and upload task

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,13 +2,12 @@
 
 set -ex
 
-cp download/server.zip docs/
-cp download/client.zip docs/
-cp download/multi.zip docs/
-
-git fetch --unshallow
+git clone --depth=1 --branch=master https://github.com/oliveiraallex/pharothings-ci.git
+cp download/server.zip pharothings-ci/docs/
+cp download/multi.zip pharothings-ci/docs/
+cp download/client.zip pharothings-ci/docs/
+cd pharothings-ci
 git checkout master
 git add docs/*.zip
-git add -u
 git commit -m "Travis upload"
 git push http://oliveiraallex:${GH}@github.com/oliveiraallex/pharothings-ci.git > /dev/null 2>&1
